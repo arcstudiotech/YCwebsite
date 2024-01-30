b4abdad2eaa9f5d058f4e9d092d309cf88be6a9e
Arcadia Tech <95274634+arcstudiotech@users.noreply.github.com>
2024-01-30 11:07:37 +1100

    Update header.jsx

--- App/components/header/header.jsx
+++ App/components/header/header.jsx
@@ -25,9 +25,9 @@ export default function Header () {
 
     return (
         <header className={`header appearance-${appearance}`}>
-            <div className="header-logo" a href="arc-studio.com.au">
+            <div className="header-logo">
                 { Logo }
-                
+                <a href="https://arc-studio.com.au"></a>
             </div>
             <div className="header-controls">
                 <div className="header-appearance" onClick={ changeAppearance } />
