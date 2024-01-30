bd9e39c149fb7018fb7c4b939dffa476cf82c2b4
Arcadia Tech <95274634+arcstudiotech@users.noreply.github.com>
2024-01-30 13:47:07 +1100

    Update header.jsx

--- App/components/header/header.jsx
+++ App/components/header/header.jsx
@@ -28,6 +28,14 @@ export default function Header () {
             <div className="header-logo" >
                 { Logo }
             </div>
+            <div class="dropdown">
+              <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                  <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                 </div>
+            </div>
             <div className="header-controls">
                 <div className="header-appearance" onClick={ changeAppearance } />
             </div>
